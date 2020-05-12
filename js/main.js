a715793f95f79147951a2d98bea97c04b7a7c12c
Kimberly Lord <kalord93@gmail.com>
2020-05-12 12:22:39 -0600

    add jquery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-// skills text color: #FFAE39
+$('form').submit(function(e){
+    var newSkill = $('input').val();
+    var addSkill = `<li><button class="remove">X</button>${newSkill}</li>`;
+        $('ul').append(addSkill); 
+        e.preventDefault();
+        $('form').trigger('reset');
+
+        $('.remove').on('click', function() {
+            $(this).parent().remove();
+        });
+    });
